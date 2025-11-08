da0a0a367c5188ad874eeea264b4d3f673730ed2
pyaephyokhant760 <pyaephyokhant2020dc@gmail.com>
2025-11-08 20:32:09 +0630

    delete

--- routes/user.js
+++ routes/user.js
@@ -5,42 +5,13 @@ const controllers = require('../controllers/user');
 
 
 
-// router.get('/',(req,res) => {
-//   res.json({con:true,msg:"User Route",result:{data:user}});
-// })
+
 
 router.get('/',controllers.all);
 router.post('/',controllers.add);
 router.patch('/',controllers.edit);
 router.delete('/:id',controllers.deleteUser);
 
-// router.post('/',(req,res) => {
-//   let newUser = req.body;
-//   user.push(newUser);
-//   res.json({con:true,msg:"User Added",result:{data:user}});
-// })
 
-// router.patch('/',(req,res,next) => {
-//   let name = req.body.name;
-//   let salary = Number(req.body.salary);
-//   let user = user.find(s => s.name === name);
-//   if(user) {
-//     user.salary = salary;
-//     res.json({con:true,msg:"User Updated",result:{data:user}})
-//   }else{
-//     next(new Error("no found"));
-//   }
-// })
-
-// router.delete('/:name', (req, res, next) => {
-//   let name = req.params.name;
-//   let userIndex = user.find(s => s.name === name);
-//   if(userIndex) {
-//     user = user.filter(s => s.name != userIndex.name);
-//     res.json({con:true,msg:"User Deleted",result:{data:user}});
-//   }else{
-//     next(new Error("no found"));
-//   }
-// })
 
 module.exports = router;
